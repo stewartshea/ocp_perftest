98c081675c1feea0774d14136c2be9481a2fb574
stewartshea <shea.stewart@arctiq.ca>
2016-10-23 16:18:02 -0400

    add wait for testing

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,11 +6,18 @@ cat /etc/phoronix-test-suite.xml
 # start service in background here
 phoronix-test-suite start-remote-gui-server
 
-echo "[hit enter key to exit] or run 'docker stop <container>'"
-read
+while [ 1 ]
+do
+    echo 'yyy'
+    sleep 5
+done
+
+#echo "[hit enter key to exit] or run 'docker stop <container>'"
+#read
+
 
 # stop service and clean up here
-echo "stopping phoronix-test-suite"
-phoronix-test-suite stop-remote-gui-server
+#echo "stopping phoronix-test-suite"
+#phoronix-test-suite stop-remote-gui-server
 
 echo "exited $0"
