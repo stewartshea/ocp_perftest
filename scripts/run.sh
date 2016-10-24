a61206651f45d1dad9f125c738c14a72f3e368f3
stewartshea <shea.stewart@arctiq.ca>
2016-10-24 11:31:18 -0400

    update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
-# Note: I've written this using sh so it works in the busybox container too
-#
-cat /etc/phoronix-test-suite.xml
 
-# start service in background here
+# Start service in background here
 phoronix-test-suite start-phoronix-server
-#phoronix-test-suite  start-remote-gui-server
 
-#while [ 1 ]
-#do
-#    echo 'yyy'
-#    sleep 5
-#done
 
 #echo "[hit enter key to exit] or run 'docker stop <container>'"
 #read
